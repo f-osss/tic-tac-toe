6af272f4cec9d61085e0d6eb9f1c05d4da9ba4bd
Faiza <salami.faiza15@gmail.com>
2024-03-03 16:30:23 -0600

    updated

--- src/main/java/com/example/tictactoe/loginController.java
+++ src/main/java/com/example/tictactoe/loginController.java
@@ -13,19 +13,22 @@ import java.io.IOException;
 
 public class loginController {
     @FXML
-    private TextField nameTextField;
+    private TextField nameTextField1;
+    @FXML
+    private TextField nameTextField2;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
     public void changeScene(ActionEvent event) throws IOException {
-        String username = nameTextField.getText();
-
+        String username1 = nameTextField1.getText();
+        String username2 = nameTextField2.getText();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("tictactoe-view.fxml"));
         root = loader.load();
-        
+
         tictactoeController tictac = loader.getController();
-        tictac.displayName(username);
+        tictac.displayName(username1,username2);
 
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
--- src/main/java/com/example/tictactoe/tictactoeController.java
+++ src/main/java/com/example/tictactoe/tictactoeController.java
@@ -5,9 +5,12 @@ import javafx.scene.control.Label;
 
 public class tictactoeController {
     @FXML
-    Label userName;
+    Label userName1;
+    @FXML
+    Label userName2;
 
-    public void displayName(String name) {
-        userName.setText(name);
+    public void displayName(String name1, String name2) {
+        userName1.setText(name1);
+        userName2.setText(name2);
     }
 }
